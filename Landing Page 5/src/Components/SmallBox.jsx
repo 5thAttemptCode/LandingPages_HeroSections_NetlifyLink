8c33c2714cc51ca48802c8a97c145ef9496a803c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-08-15 19:24:46 +0200

    Add files via upload

--- Landing Page 5/src/Components/SmallBox.jsx
+++ Landing Page 5/src/Components/SmallBox.jsx
@@ -4,18 +4,18 @@ import "./css/big-box.css"
 export default function SmallBox() {
 
     const [ infos, setInfos ] = useState([
-        {title: "Hello", text: "hello again", textTwo:"Whatsup", id: 1},
-        {title: "Hello2", text: "hello again2", textTwo:"Whatsup2", id: 2},
+        {title: "21.8Mil+", text: "Analysis", textThree:"Based on observation and existing knodledge scientists prpose e testable explanation or hypothesis to answer. ", id: 1},
+        {title: "2024", text: "Community", textThree:"New evidence and discoveries can lead to the modification or rejection of existing theoris and the development.", id: 2},
     ])
 
 
   return (
-    <div className="hero">
+    <div className="small-wrapper">
         {infos.map((info) => (
             <div className="small-box box" key={info.id}>
                 <h1>{info.title}</h1>
-                <p>{info.text}</p>
-                <h3>{info.textTwo}</h3>
+                <h3>{info.text}</h3>
+                <p>{info.textThree}</p>
             </div>
         ))}
     </div>
