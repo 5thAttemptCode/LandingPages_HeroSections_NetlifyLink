95431a8ee00891e662bbfc7bde6afbe4d17101b2
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-09-29 20:56:55 +0200

    Add files via upload

--- Landing Page 5/src/ProductSlider.jsx
+++ Landing Page 5/src/ProductSlider.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { SMOOTHIES } from './Smoothies'
 import ProductLink from './ProductLink'
+import { motion } from 'framer-motion'
+
 
 export default function ProductSlider() {
+
+  const [ width, setWidth ] = useState(0)
+  const carouselWidth = useRef()
+
+  useEffect(() => {
+    console.log(carouselWidth.current.offsetWidth);
+  }, [])
   
   return (
-    <div className='product-slider'>
-      {SMOOTHIES.map((smoothy) => {
-        return( 
-        <ProductLink key={smoothy.id} smoothy={smoothy} />
-        )
-      })}
-    </div>
+    <motion.div className='product-slider carousel' ref={carouselWidth} >
+      <motion.div drag="x" dragConstraints={{ right: 0}} className='inner-carousel'>
+        {SMOOTHIES.map((smoothy) => {
+          return( 
+          <motion.div className='item' key={smoothy.id}>
+            <ProductLink smoothy={smoothy} />
+          </motion.div>
+          )
+        })}
+      </motion.div>
+    </motion.div>
   )
 }
