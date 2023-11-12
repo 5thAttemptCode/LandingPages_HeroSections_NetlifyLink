6a9e40b1c7a9942f6cfcf61ad3897ce27207365b
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-12 20:13:20 +0100

    Update firebase.config.js

--- Landing Page 5/src/utils/firebase.config.js
+++ Landing Page 5/src/utils/firebase.config.js
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA0ldtQj9-katG-SF-tQxPcNhmaUFby7-g",
-    authDomain: "crud-f64da.firebaseapp.com",
-    projectId: "crud-f64da",
-    storageBucket: "crud-f64da.appspot.com",
-    messagingSenderId: "744135190318",
-    appId: "1:744135190318:web:b9dc139fee0dc50a25bcbf"
+    apiKey: 
+    authDomain: 
+    projectId: 
+    storageBucket: 
+    messagingSenderId: 
+    appId:
 };
 
 // Initialize Firebase
