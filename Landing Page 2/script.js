5da585996f62388da82829b9f57ab269a409654b
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-23 20:44:18 +0100

    Add files via upload

--- Landing Page 2/script.js
+++ Landing Page 2/script.js
@@ -1,83 +1,28 @@
 gsap.registerPlugin(ScrollTrigger);
 
-var tl = gsap.timeline();
-
-const vh = (coef) => window.innerHeight * (coef/100);
-
-
-tl.to("#home", {
+gsap.to(".box", {
+    left: "-50%",
     scrollTrigger: {
-        trigger: "#home",
-        pin: true,
+        trigger:"#container",
         start: "top top",
-        end: vh(220) + "top"
-    }
-})
-
-tl.to(".box_one", {
-    width: "50%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(5) + "top",
-        end: vh(100) + "top",
-        scrub: true,
-        toggleActions: "restart reverse restart reverse",
-    }
-})
-
-tl.to(".box_two", {
-    height: "30.1%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(100) + "top",
-        end: vh(120) + "top",
-        scrub: true,
-        toggleActions: "restart reverse restart reverse",
-    }
-})
-
-tl.to(".box_three", {
-    height: "70%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(120) + "top",
-        end: vh(140) + "top",
-        scrub: true,
-        toggleActions: "restart reverse restart reverse",
-    }
-})
-
-tl.to(".box_four", {
-    width: "30%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(140) + "top",
-        end: vh(160) + "top",
-        scrub: true,
-        toggleActions: "restart reverse restart reverse",
-    }
-})
-
-tl.to(".box_five", {
-    height: "30%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(160) + "top",
-        end: vh(180) + "top",
-        scrub: true,
-        toggleActions: "restart reverse restart reverse",
-    }
-})
-
-tl.to(".box_six", {
-    width: "15%",
-    scrollTrigger: {
-        trigger:".box_one",
-        start: vh(180) + "top",
-        end: vh(200) + "top",
-        scrub: true,
+        pin: true,
+        scrub: true, 
         toggleActions: "restart reverse restart reverse",
     }
 })
 
 
+//DATE
+const currentDate = new Date()
+const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }
+const dateDisplay = currentDate.toLocaleDateString(undefined, options)
+document.getElementById('todayDate').textContent = dateDisplay
+
+
+//TIME
+const updateTime = () => {
+    const now = new Date()
+    const time = now.toLocaleTimeString()
+    document.getElementById('currentTime').textContent = time
+};
+setInterval(updateTime, 1000)
